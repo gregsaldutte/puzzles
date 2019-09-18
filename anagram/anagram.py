0d5905cc3b4ff33ca15443f2762b750ed78c3c9f
Sixun Tang <tangsixun1997@gmail.com>
2019-09-18 19:44:14 +0000

    change again

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -2,7 +2,7 @@ def find_anagrams(word, candidates):
     cand_list = []
     word_set = set(word.lower())
     for cand in candidates:
-        cand_set = set(cand)
+        cand_set = set(cand.lower())
         if cand_set==word_set and len(word)==len(cand) and word.lower()!=cand.lower():
             cand_list.append(cand)
     
