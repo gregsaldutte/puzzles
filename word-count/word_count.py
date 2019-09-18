b2a1a6824b1b3759aeb7cbb4e75cd397124b3133
Sixun Tang <tangsixun1997@gmail.com>
2019-09-18 18:15:17 +0000

    Matrix Solved

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -4,9 +4,7 @@ def count_words(sentence):
     word_list = sentence.lower().split()
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     number = '0123456789'
-    print(word_list)
     for item in word_list:
-        print(item)
         item_list = []
         for i in range(len(item)):
             if item[i] in alphabet or item[i] in number:
@@ -14,7 +12,6 @@ def count_words(sentence):
             elif 0<i<len(item)-1:
                 if item[i-1] in alphabet and item[i+1] in alphabet:
                     item_list.append(item[i])
-        print(item_list)
         word_final_list.append(''.join(item_list))
     for item in word_final_list:
         if item not in word_count_dict:
