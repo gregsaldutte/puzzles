a0b13786279da1df9da31c5ec2d92fe9efcc15c7
Sixun Tang <tangsixun1997@gmail.com>
2019-09-18 19:57:23 +0000

    Anagram lol

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,9 +1,11 @@
 def find_anagrams(word, candidates):
     cand_list = []
-    word_list = list(word.lower()).sort()
+    word_list = list(word.lower())
+    word_list = word_list.sort()
     print(word_list)
     for cand in candidates:
-        cand_list_temp = list(cand.lower()).sort()
+        cand_list_temp = list(cand.lower())
+        cand_list_temp = cand_list_temp.sort()
         print(cand_list_temp)
         # The anagram should be of the same letter, and each letter appears for the same time, and could not be itself
         # Case insensitive
