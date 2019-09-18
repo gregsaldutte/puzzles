6733c4d57519fcb69e0de5db2c47ed050a3562b6
Sixun Tang <tangsixun1997@gmail.com>
2019-09-18 19:48:03 +0000

    New Anagram

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,9 +1,11 @@
 def find_anagrams(word, candidates):
     cand_list = []
-    word_set = set(word.lower())
+    word_list = list(word.lower()).sort()
     for cand in candidates:
-        cand_set = set(cand.lower())
-        if cand_set==word_set and len(word)==len(cand) and word.lower()!=cand.lower():
+        cand_list = list(cand.lower()).sort()
+        # The anagram should be of the same letter, and each letter appears for the same time, and could not be itself
+        # Case insensitive
+        if cand_list==word_list and len(word)==len(cand) and word.lower()!=cand.lower():
             cand_list.append(cand)
     
     return cand_list
