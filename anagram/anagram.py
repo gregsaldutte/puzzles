4e2c594c51b11c62eef881e76dc305a1646a7596
Sixun Tang <tangsixun1997@gmail.com>
2019-09-18 19:49:05 +0000

    New Anagram

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -2,10 +2,10 @@ def find_anagrams(word, candidates):
     cand_list = []
     word_list = list(word.lower()).sort()
     for cand in candidates:
-        cand_list = list(cand.lower()).sort()
+        cand_list_temp = list(cand.lower()).sort()
         # The anagram should be of the same letter, and each letter appears for the same time, and could not be itself
         # Case insensitive
-        if cand_list==word_list and len(word)==len(cand) and word.lower()!=cand.lower():
+        if cand_list_temp==word_list and len(word)==len(cand) and word.lower()!=cand.lower():
             cand_list.append(cand)
     
     return cand_list
