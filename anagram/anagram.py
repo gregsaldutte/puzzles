1b29a83a8f443caa4932acaf14ef05db94b2fa8d
Sixun Tang <tangsixun1997@gmail.com>
2019-09-18 19:42:31 +0000

    Anagram changed a bit to fit the test

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,11 +1,10 @@
 def find_anagrams(word, candidates):
     cand_list = []
-    word_set = set(word)
+    word_set = set(word.lower())
     for cand in candidates:
         cand_set = set(cand)
-        if cand_set==word_set and len(word)==len(cand):
+        if cand_set==word_set and len(word)==len(cand) and word.lower()!=cand.lower():
             cand_list.append(cand)
     
     return cand_list
-
 
